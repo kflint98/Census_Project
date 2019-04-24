d4f8b6c84d2ce4ca8e28b600d5d66bad9df36b46
austinatchley1 <36238933+austinatchley1@users.noreply.github.com>
2019-04-24 15:00:25 -0500

    Update Server.R

--- Shiny/Server.R
+++ Shiny/Server.R
@@ -1,6 +1,4 @@
-###section 1 - Runs at start of program
 library(class)
-#library(stats)
 library(graphics)
 library(caret)
 library(InformationValue)
@@ -11,10 +9,9 @@ adult <- adult[-c(1)]
 adult <-na.exclude(adult)
 
 n = nrow(adult)
-###
+
 trainIndex = sample(1:n, size = round(0.8*n), replace=FALSE)
 train = adult[trainIndex ,]
-#validation = adult[-trainIndex ,]
 notTrain = adult[-trainIndex ,]
 n = nrow(notTrain) ###
 validationIndex = sample(1:n, size = round(0.5*n), replace=FALSE) ###
@@ -30,17 +27,7 @@ validation<-validation[-c(15)]
 
 missclassificationRate <- c() 
 
-# ### added
-# trctrl<-trainControl(method='repeatedcv', number=10, repeats = 3)
-# knnFit<-train(as.factor(income)~., data=train, method='knn', trControl=trctrl, tuneLength=10)
-# #extract the k value
-# temp<-knnFit$bestTune
-# result[i,1]<-temp$k
-# #run knn
-# ###
-
-#for (k in 1:20) {
-for (k in 1:20) { #should this be above to repartition
+for (k in 1:20) {
   predicted.labels <- knn(train, validation, cl_train, k)
   
   count <- 0
@@ -57,11 +44,8 @@ for (k in 1:20) { #should this be above to repartition
 }
 kValues <- c(1:20)
 
-###section 2
 server <- function(input, output) {
   
-  #output$distPlot <- reactive(renderPlot({
-  #observeEvent(input$range,
   output$distPlot <- renderPlot({
     
     min <- input$range[1]
@@ -77,7 +61,6 @@ server <- function(input, output) {
     )
     
     if (input$var == "K-NN") {
-    #barplot
     #plot accuracy
     plot(1-missclassificationRate[min:max],names.arg=kValues[min:max],xlab="Number of Neighbors",
             ylab="Accuracy",type = "b",col="blue",
@@ -115,26 +98,8 @@ server <- function(input, output) {
       plotROC(test$income, pred_vals)
     }
     
-    # barplot(missclassificationRate[min:max],names.arg=kValues[min:max],xlab="Number of Neighbors",
-    #      ylab="Misclassification Rate",col="blue",
-    #      main="Misclassification Rate by Number of Nearest Neighbors",border="black")
-    # }
-    
-    # if (input$var == "K-Means") {
-    #   clusters <- kmeans(na.omit(adult), 2)
-    #   temp<-data.frame(na.omit(adult), cluster=factor(clusters$cluster))
-    #   ggplot(temp, aes(x=fnlwgt, y=capital.loss, color=cluster))+geom_point()
-    # }
-    
-    # if (input$var == "Logistic Regression") {
-    #   logitMod <- glm(income ~ age+workclass+fnlwgt+education.num+marital.status+occupation+
-    #                     relationship+race+sex+capital.gain+capital.loss+hours.per.week+native.country,
-    #                   data=train, family=binomial(link='logit'), na.action = na.exclude)
-    # }
-    
   }
-  #})
-  #)
+
   )
   
 }
